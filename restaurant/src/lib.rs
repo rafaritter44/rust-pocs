5ce402447dafee7c9004b40490f6b0bd8b17545d
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-05-21 11:14:18 -0300

    Bring path into scope

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -44,12 +44,18 @@ mod back_of_house {
     }
 }
 
+// Bring path into scope
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Path brought into scope
+    hosting::add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
