776ff891485123dd5db3b510db032b15bdeb9575
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-05-29 13:40:15 -0300

    Improve variable names

--- template_renderer/src/main.rs
+++ template_renderer/src/main.rs
@@ -11,16 +11,16 @@ fn main() {
 
     let input_json = match fs::read_to_string(input_path) {
         Ok(input_json) => input_json,
-        Err(error) => {
-            eprintln!("Error reading file '{}': {}.", input_path.display(), error);
+        Err(e) => {
+            eprintln!("Error reading file '{}': {}.", input_path.display(), e);
             return;
         }
     };
 
     let table: Table = match serde_json::from_str(&input_json) {
         Ok(table) => table,
-        Err(error) => {
-            eprintln!("Error parsing JSON: {}.", error);
+        Err(e) => {
+            eprintln!("Error parsing JSON: {}.", e);
             return;
         }
     };
@@ -31,11 +31,11 @@ fn main() {
     ];
 
     for renderer in renderers.iter() {
-        let rendered = renderer.render(&table);
-        let output_file = format!("output.{}", renderer.file_extension());
-        let output_path = Path::new(&output_file);
+        let rendered_content = renderer.render(&table);
+        let output_filename = format!("output.{}", renderer.file_extension());
+        let output_path = Path::new(&output_filename);
 
-        match rendered {
+        match rendered_content {
             RenderedContent::Text(content) => {
                 if let Err(e) = fs::write(output_path, content) {
                     eprintln!("Error writing to '{}': {}.", output_path.display(), e);
