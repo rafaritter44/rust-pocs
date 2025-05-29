c80d0480a394a1f32ff015ca64ed5e31405d8245
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-05-29 16:26:57 -0300

    Use HTML Renderer inside PDF Renderer

--- template_renderer/src/renderers/pdf.rs
+++ template_renderer/src/renderers/pdf.rs
@@ -3,13 +3,18 @@ use std::collections::BTreeMap;
 use crate::table::Table;
 use super::renderer::Renderer;
 use super::rendered_content::RenderedContent;
+use super::html::HtmlRenderer;
 use printpdf::*;
 
 pub struct PdfRenderer;
 
 impl Renderer for PdfRenderer {
     fn render(&self, table: &Table) -> RenderedContent {
-        let html = "";
+        let html_renderer = HtmlRenderer;
+        let RenderedContent::Text(html) = html_renderer.render(table) else {
+            panic!("HtmlRenderer should only return text.");
+        };
+
         let images = BTreeMap::new();
         let fonts = BTreeMap::new();
         let options = GeneratePdfOptions {
@@ -17,7 +22,7 @@ impl Renderer for PdfRenderer {
         };
         let mut warnings = Vec::new();
 
-        let pdf_bytes = PdfDocument::from_html(html, &images, &fonts, &options, &mut warnings)
+        let pdf_bytes = PdfDocument::from_html(&html, &images, &fonts, &options, &mut warnings)
             .unwrap()
             .save(&PdfSaveOptions::default(), &mut warnings);
 
