31bfa12449e10b5d28aedf92e2d897631c7d4f83
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-06-25 11:24:29 -0300

    Add ticket system constructor

--- ticket_system/src/system.rs
+++ ticket_system/src/system.rs
@@ -1,4 +1,17 @@
-struct TicketSystem {
+use std::collections::{HashMap, HashSet};
+use chrono::NaiveDate;
+use crate::models::{Venue, Zone, Show, Ticket};
+
+pub struct TicketSystem {
     shows: HashMap<usize, Show>,
     next_show_id: usize,
+}
+
+impl TicketSystem {
+    pub fn new() -> Self {
+        TicketSystem {
+            shows: HashMap::new(),
+            next_show_id: 1,
+        }
+    }
 }
