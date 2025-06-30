7c7e3c72979593d92fdbcf0ece802d3a619c8a28
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-06-30 13:06:43 -0300

    Implement add_show()

--- ticket_system/src/models.rs
+++ ticket_system/src/models.rs
@@ -3,31 +3,31 @@ use chrono::NaiveDate;
 
 #[derive(Debug)]
 pub struct Venue {
-    name: String,
-    zones: HashMap<String, Zone>, // zone name -> Zone
-    max_capacity: usize,
+    pub name: String,
+    pub zones: HashMap<String, Zone>, // zone name -> Zone
+    pub max_capacity: usize,
 }
 
 #[derive(Debug)]
 pub struct Zone {
-    name: String,
-    seat_count: usize,
-    price: f32,
+    pub name: String,
+    pub seat_count: usize,
+    pub price: f32,
 }
 
 #[derive(Debug)]
 pub struct Show {
-    id: usize,
-    title: String,
-    date: NaiveDate,
-    venue: Venue,
-    tickets_sold: HashMap<String, HashSet<usize>>, // zone name -> seat numbers sold
+    pub id: usize,
+    pub title: String,
+    pub date: NaiveDate,
+    pub venue: Venue,
+    pub tickets_sold: HashMap<String, HashSet<usize>>, // zone name -> seat numbers sold
 }
 
 #[derive(Debug)]
 pub struct Ticket {
-    show_id: usize,
-    zone: String,
-    seat_number: usize,
-    buyer_name: String,
+    pub show_id: usize,
+    pub zone: String,
+    pub seat_number: usize,
+    pub buyer_name: String,
 }
--- ticket_system/src/system.rs
+++ ticket_system/src/system.rs
@@ -14,4 +14,17 @@ impl TicketSystem {
             next_show_id: 1,
         }
     }
+    pub fn add_show(&mut self, title: String, date: NaiveDate, venue: Venue) -> usize {
+        let show_id = self.next_show_id;
+        let show = Show {
+            id: show_id,
+            title,
+            date,
+            venue,
+            tickets_sold: HashMap::new(),
+        };
+        self.shows.insert(show_id, show);
+        self.next_show_id += 1;
+        show_id
+    }
 }
