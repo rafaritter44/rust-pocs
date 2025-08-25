46c8c4a4596444e4272dc7fc546811ef328b8eb1
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-08-25 11:34:53 -0300

    Make test pass

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,13 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
