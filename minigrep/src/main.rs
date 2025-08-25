6b861b7649f27f8cf940b881b6d71880351b6b50
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-08-25 11:31:08 -0300

    Create lib search function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,6 +3,8 @@ use std::error::Error;
 use std::fs;
 use std::process;
 
+use minigrep::search;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -23,7 +25,9 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
