cf6ed05014b768fedcef023386fe6bd5cee546c3
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-10-03 17:42:07 -0300

    Send HTTP response

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<_> = buf_reader
         .lines()
@@ -21,5 +21,7 @@ fn handle_connection(stream: TcpStream) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {http_request:#?}");
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
