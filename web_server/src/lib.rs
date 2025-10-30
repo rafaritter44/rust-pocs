2b39e170c8be6ec75be9a4be0ba12b26c2dee429
Rafael Barni Ritter <rafaritter44@hotmail.com>
2025-10-30 10:54:03 -0300

    Explicitly breaking out of the loop when recv returns an error

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -60,11 +60,19 @@ impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
+                let message = receiver.lock().unwrap().recv();
 
-                println!("Worker {id} got a job; executing.");
+                match message {
+                    Ok(job) => {
+                        println!("Worker {id} got a job; executing.");
 
-                job();
+                        job();
+                    }
+                    Err(_) => {
+                        println!("Worker {id} disconnected; shutting down.");
+                        break;
+                    }
+                }
             }
         });
 
